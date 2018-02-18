3a2a072b1fddaf472d66c570e7cefbba45a304bd
Chris Guy <christien.guy@theunit.co.uk>
2018-02-18 00:16:45 +0000

    Add chevrons to dropdowns

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Select.css";
+import chevron from "../icons/chevron-down.svg";
 
 export const Select = ({ options, onChange, value }) => (
-  <select className="select" value={value} onChange={onChange}>
+  <select style={{ backgroundImage: `url(${chevron})` }} className="select" value={value} onChange={onChange}>
     {options.map((option, index) => (
       <option key={index} value={option.value}>
         {option.name}
